98e32aab795c49333df424035ac9e0282f71449b
Mauro Salomão <oruamsalomao@gmail.com>
2021-07-12 20:31:30 -0300

    Projeto inicial.

---
+++ src/test/java/estoque/cerveja/CervejaApplicationTests.java
@@ -0,0 +1,13 @@
+package estoque.cerveja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CervejaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
